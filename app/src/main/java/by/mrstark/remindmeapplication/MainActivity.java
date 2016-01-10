04cbe59874f820d02d63e4711e550ae3547f76ab
mrstark <lmasliakov@gmail.com>
2016-01-10 19:06:07 +0300

    Start to implementing drawerview

--- app/src/main/java/by/mrstark/remindmeapplication/MainActivity.java
+++ app/src/main/java/by/mrstark/remindmeapplication/MainActivity.java
@@ -2,18 +2,20 @@ package by.mrstark.remindmeapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 /**
  * Created by mrstark on 9.1.16.
  */
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity  {
 
     private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
